022646af716c2a111c85d2de861989fe1be50a7d
yuribaek <chsem145@naver.com>
2021-02-04 20:20:25 +0900

    0204 study

--- src/BOJ_2493_탑.java
+++ src/BOJ_2493_탑.java
@@ -8,54 +8,44 @@ public class BOJ_2493_탑 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		Stack<Integer> tops = new Stack<>();
 		Stack<Integer> topIdx = new Stack<>();
+		Stack<Integer> topsNum = new Stack<>();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int num = Integer.parseInt(br.readLine());
-		
+
 		String s = br.readLine();
+		
 		StringTokenizer st = new StringTokenizer(s);
-		int before = Integer.parseInt(st.nextToken());
-		int top = Integer.parseInt(st.nextToken());
+		int before = Integer.parseInt(st.nextToken());// 1
+		int top ;
 		int nowIdx = 1;
+		topIdx.add(0);
+		tops.add(before);
+		topsNum.add(1);
 		while (st.hasMoreTokens()) {
-			if(before>top) {
-				tops.push(before);
-				tops.push(nowIdx++);
-				before = top;
-				top = Integer.parseInt(st.nextToken());
+			top = Integer.parseInt(st.nextToken());
+			nowIdx++;
+			while (!tops.isEmpty() &&tops.peek() <= top) {
+				tops.pop();
+				topsNum.pop();
+				
 			}
+			if(tops.isEmpty()) {
+				topIdx.push(0);
+				tops.push(top);
+				topsNum.add(nowIdx);
+			}else {
+				tops.push(top);
+				topIdx.push(topsNum.peek());
+				topsNum.add(nowIdx);
+				
+			}
+
 			
 		}
-		
-//		int nowTop;
-//		int temp;
-//		while (!tops.isEmpty()) {
-//			nowTop = tops.pop();
-//			while (true) {
-//				if (tops.isEmpty()) {
-//					result[--num] = 0;
-//					while (!tempTops.isEmpty()) {
-//						tops.push(tempTops.pop());
-//					}
-//					break;
-//				}
-////				int temp = tops.pop();
-//				
-//				if (nowTop <= tops.peek()) {
-////					tops.push(temp);
-//					result[--num] = tops.size();
-//					while (!tempTops.isEmpty()) {
-//						tops.push(tempTops.pop());
-//					}
-//					break;
-//				} else {
-//					temp=tops.pop();
-//					tempTops.push(temp);
-//				}
-//			}
-//		}
+
 		StringBuilder sb = new StringBuilder();
-		for(int a: topIdx) {
+		for (int a : topIdx) {
 			sb.append(a).append(" ");
 		}
 		System.out.println(sb);
