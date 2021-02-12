ae56225c4091644eb8ef868b1d4f1fda2d3396ac
yuribaek <chsem145@naver.com>
2021-02-12 20:36:07 +0900

    0212

--- src/BOJ_1106_호텔.java
+++ src/BOJ_1106_호텔.java
@@ -2,8 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,49 +18,31 @@ public class BOJ_1106_호텔 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		C = Integer.parseInt(st.nextToken());// 최소 인원
 		N = Integer.parseInt(st.nextToken());// 도시 개수
+		int[] dp = new int[2001];
+		Arrays.fill(dp, Integer.MAX_VALUE);
+		dp[0] = 0;
 		city = new ArrayList<Integer>();
 		cityMap = new HashMap<>();
 		for (int n = 0; n < N; n++) {
 			st = new StringTokenizer(br.readLine());
-			int b =  Integer.parseInt(st.nextToken());
-			int a =  Integer.parseInt(st.nextToken());
-			if(cityMap.containsKey(a) && cityMap.get(a)<b) {
-				continue;
+			int b =  Integer.parseInt(st.nextToken()); // 고객을 모으는 비용 : value
+			int a =  Integer.parseInt(st.nextToken()); // 고객의 수 : key
+
+			
+			for(int j=0;j<=2000;j++) {
+				if(dp[j]== Integer.MAX_VALUE) continue;
+				for(int k=0;j+k*a<=2000;k++) {
+					dp[j+k*a] = Integer.min(dp[j+k*a], dp[j]+k*b);
+				}
 			}
-			cityMap.put(a, b);
+			
 		}
-		city.addAll(cityMap.keySet());
 		
-		Collections.sort(city,new Comparator<Integer>() {
-			@Override
-			public int compare(Integer o1, Integer o2) {
-				// TODO Auto-generated method stub
-				return o2-o1;
-			}
-		});
-		choise(0,C,0);
-		
+		for(int i=C;i<=2000;i++) {
+			minValue = Integer.min(minValue, dp[i]);
+		}
+
 		System.out.println(minValue);
 	}
 
-	static void choise(int index,int nowPerson,int nowMoney) {
-		if(index == city.size() ||nowPerson<=0 ||nowMoney>=minValue) {
-			return;
-		}
-		int first = city.get(index); //현재 인덱스 고객의 수
-		int number = nowPerson / first;
-		number = nowPerson % first != 0 ? number + 1 : number; //최대 고를수 있는 숫자
-		
-		int money =cityMap.get(first);
-		for(int i=number;i>=0;i--) {
-			
-			int tempPerson = first*i;
-			if(nowPerson<=tempPerson) {
-				minValue = Integer.min(minValue, nowMoney + (i * money));
-				continue;
-			}else {
-				choise(index+1, nowPerson-tempPerson, nowMoney + (i * money));
-			}
-		}
-	}
 }
