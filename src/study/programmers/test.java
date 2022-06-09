3ae55ec28e784725124052aed8dff56bd9b3a5c4
DESKTOP-5RRNCP0\Yuri <chsem145@naver.com>
2022-06-09 23:00:01 +0900

    0609

--- src/study/programmers/test.java
+++ src/study/programmers/test.java
@@ -2,4 +2,10 @@ package study.programmers;
 
 public class test {
 
+	public static void main(String[] args) {
+		
+		System.out.println("hello world");
+		
+		System.out.println("hello people");
+	}
 }
